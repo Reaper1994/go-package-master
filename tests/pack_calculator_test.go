5cf2751e5a02f2a92a57dc3800891d4df388f381
osmar <osmarrod18@gmail.com>
2024-07-28 01:30:16 +0100

    seperated testcase

--- tests/pack_calculator_test.go
+++ tests/pack_calculator_test.go
@@ -1,44 +1,100 @@
-package services
+package services_test
 
 import (
-	"sort"
+	"log"
+	"testing"
 
+	"github.com/Reaper1994/go-package-master/internal/config"
 	"github.com/Reaper1994/go-package-master/internal/models"
+	"github.com/Reaper1994/go-package-master/internal/services"
 )
 
-type PackCalculatorV1 struct {
-	Packs []models.Pack
-}
+var calculator services.PackCalculatorV1
 
-// CalculatePacks calculates the optimal combination of packs.
-func (pc *PackCalculatorV1) CalculatePacks(order models.Order) []models.Pack {
-	// Sort packs in descending order to prioritize larger packs first
-	sort.Slice(pc.Packs, func(i, j int) bool {
-		return pc.Packs[i].Size > pc.Packs[j].Size
-	})
-
-	var calculatePacksFromRequestedItems func(int, []models.Pack) []models.Pack
-	calculatePacksFromRequestedItems = func(itemsOrdered int, res []models.Pack) []models.Pack {
-		if itemsOrdered == 0 {
-			return res
-		}
-
-		for _, pack := range pc.Packs {
-			if itemsOrdered >= pack.Size {
-				res = append(res, pack)
-				itemsOrdered -= pack.Size
-				return calculatePacksFromRequestedItems(itemsOrdered, res)
-			}
-		}
-
-		// If remaining items are less than the smallest pack size, add the smallest pack
-		if itemsOrdered > 0 {
-			smallestPack := pc.Packs[len(pc.Packs)-1]
-			res = append(res, smallestPack)
-		}
-
-		return res
+func init() {
+	cfg, err := config.LoadConfig("../cmd/config.json") // adjust the path as needed
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
 	}
 
-	return calculatePacksFromRequestedItems(order.Items, []models.Pack{})
+	var packs []models.Pack
+	for _, p := range cfg.Packs {
+		packs = append(packs, models.Pack{Size: p.Size})
+	}
+
+	calculator = services.PackCalculatorV1{
+		Packs: packs,
+	}
+}
+
+func TestPackCalculatorV1_CalculatePacks_1(t *testing.T) {
+	order := models.Order{Items: 1}
+	expectedPacks := []models.Pack{{Size: 250}}
+
+	packs := calculator.CalculatePacks(order)
+	if !equalPacks(packs, expectedPacks) {
+		t.Errorf("Expected packs %v, but got %v", expectedPacks, packs)
+	} else {
+		t.Log("TestPackCalculatorV1_CalculatePacks_1: Success")
+	}
+}
+
+func TestPackCalculatorV1_CalculatePacks_250(t *testing.T) {
+	order := models.Order{Items: 250}
+	expectedPacks := []models.Pack{{Size: 250}}
+
+	packs := calculator.CalculatePacks(order)
+	if !equalPacks(packs, expectedPacks) {
+		t.Errorf("Expected packs %v, but got %v", expectedPacks, packs)
+	} else {
+		t.Log("TestPackCalculatorV1_CalculatePacks_250: Success")
+	}
+}
+
+func TestPackCalculatorV1_CalculatePacks_251(t *testing.T) {
+	order := models.Order{Items: 251}
+	expectedPacks := []models.Pack{{Size: 500}}
+
+	packs := calculator.CalculatePacks(order)
+	if !equalPacks(packs, expectedPacks) {
+		t.Errorf("Expected packs %v, but got %v", expectedPacks, packs)
+	} else {
+		t.Log("TestPackCalculatorV1_CalculatePacks_251: Success")
+	}
+}
+
+func TestPackCalculatorV1_CalculatePacks_501(t *testing.T) {
+	order := models.Order{Items: 501}
+	expectedPacks := []models.Pack{{Size: 500}, {Size: 250}}
+
+	packs := calculator.CalculatePacks(order)
+	if !equalPacks(packs, expectedPacks) {
+		t.Errorf("Expected packs %v, but got %v", expectedPacks, packs)
+	} else {
+		t.Log("TestPackCalculatorV1_CalculatePacks_501: Success")
+	}
+}
+
+func TestPackCalculatorV1_CalculatePacks_12001(t *testing.T) {
+	order := models.Order{Items: 12001}
+	expectedPacks := []models.Pack{{Size: 5000}, {Size: 5000}, {Size: 2000}, {Size: 250}}
+
+	packs := calculator.CalculatePacks(order)
+	if !equalPacks(packs, expectedPacks) {
+		t.Errorf("Expected packs %v, but got %v", expectedPacks, packs)
+	} else {
+		t.Log("TestPackCalculatorV1_CalculatePacks_12001: Success")
+	}
+}
+
+func equalPacks(a, b []models.Pack) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i].Size != b[i].Size {
+			return false
+		}
+	}
+	return true
 }
