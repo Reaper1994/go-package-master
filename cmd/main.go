545ccbd365bfb6324aedd9800d7723374b77f11e
osmar <osmarrod18@gmail.com>
2024-07-28 14:29:02 +0100

    minor refactor

--- cmd/main.go
+++ cmd/main.go
@@ -1,31 +1,38 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 
-    "github.com/Reaper1994/go-package-master/internal/config"
-    v1 "github.com/Reaper1994/go-package-master/internal/handlers/v1"
-    "github.com/Reaper1994/go-package-master/internal/middleware"
-    "github.com/Reaper1994/go-package-master/internal/services"
+	"github.com/Reaper1994/go-package-master/internal/config"
+	v1 "github.com/Reaper1994/go-package-master/internal/handlers/v1"
+	"github.com/Reaper1994/go-package-master/internal/middleware"
+	"github.com/Reaper1994/go-package-master/internal/models"
+	"github.com/Reaper1994/go-package-master/internal/services"
 )
 
 func main() {
-    cfg, err := config.LoadConfig("config.json")
-    if err != nil {
-        log.Fatalf("Error loading config: %v", err)
-    }
+	cfg, err := config.LoadConfig("config.json")
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
 
-    // Initialize pack calculators for each version
-        calculatorV1 := services.PackCalculatorV1{Packs: cfg.Packs}
+	// Convert cfg.Packs to []models.Pack
+	var packs []models.Pack
+	for _, p := range cfg.Packs {
+		packs = append(packs, models.Pack{Size: p.Size})
+	}
 
-        // Handlers for each version
-        handlerV1 := &v1.CalculateHandlerV1{Calculator: calculatorV1}
+	// Initialize pack calculators for each version
+	calculatorV1 := services.PackCalculatorV1{Packs: packs}
 
-        // Set up routes with middleware
-        http.Handle("/api/v1/calculate", middleware.LoggingMiddleware(middleware.RecoveryMiddleware(handlerV1)))
+	// Handlers for each version
+	handlerV1 := &v1.CalculateHandlerV1{Calculator: calculatorV1}
 
-        fmt.Println("PackMaster server is running on port 8080")
-        log.Fatal(http.ListenAndServe(":8080", nil))
+	// Set up routes with middleware
+	http.Handle("/api/v1/calculate", middleware.LoggingMiddleware(middleware.RecoveryMiddleware(handlerV1)))
+
+	fmt.Println("PackMaster server is running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
