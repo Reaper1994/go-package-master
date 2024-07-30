0e960be5c0a22fa5b1b69f47cf98de9da6e1903b
osmar <osmarrod18@gmail.com>
2024-07-30 16:38:50 +0100

    minor refactor

--- internal/handlers/v1/calculate.go
+++ internal/handlers/v1/calculate.go
@@ -15,15 +15,26 @@ type CalculateHandlerV1 struct {
 
 // ServeHTTP processes the request and responds with the calculated packs.
 func (h *CalculateHandlerV1) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
 	var order models.Order
-	if err := json.NewDecoder(r.Body).Decode(&order); err != nil {
-		http.Error(w, "Invalid request payload", http.StatusBadRequest)
-		return
-	}
+
+    // Check Accept header
+        acceptHeader := r.Header.Get("Accept")
+        if !strings.Contains(acceptHeader, "application/json") {
+            http.Error(w, "Unsupported Media Type", http.StatusUnsupportedMediaType)
+            return
+        }
 
 	packs := h.Calculator.CalculatePacks(order)
 
-	w.Header().Set("Content-Type", "application/json")
+    // Set security headers
+    w.Header().Set("X-Frame-Options", "DENY")
+    w.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'")
+    w.Header().Set("X-Content-Type-Options", "nosniff")
+    w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
+    w.Header().Set("Allow", "POST")
+    w.Header().Set("Content-Type", "application/json")
+
 	if err := json.NewEncoder(w).Encode(packs); err != nil {
 		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
 	}
