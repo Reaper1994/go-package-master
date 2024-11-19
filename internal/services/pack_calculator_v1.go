3888d2cb430d59da46e6efea80268803b7d2b1a9
osmarrodrigues <osmarrod18@gmail.com>
2024-11-19 22:10:33 +0000

    optimized to handle calculations concurrently

--- internal/services/pack_calculator_v1.go
+++ internal/services/pack_calculator_v1.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"sort"
+	"sync"
 
 	"github.com/Reaper1994/go-package-master/internal/models"
 )
@@ -9,18 +10,30 @@ import (
 type PackCalculatorV1 struct {
 	Packs       []models.Pack // Original Packs
 	SortedPacks []models.Pack // Cached Sorted Packs
+	mu          sync.Mutex    // Mutex to protect SortedPacks
 }
 
 // SortAndCachePacks sorts the Packs slice in descending order and stores the result.
 func (pc *PackCalculatorV1) SortAndCachePacks() {
+	pc.mu.Lock()
+	defer pc.mu.Unlock()
+
 	// Copy the original packs to avoid modifying the original order
 	pc.SortedPacks = make([]models.Pack, len(pc.Packs))
 	copy(pc.SortedPacks, pc.Packs)
 
-	// Sort the copied packs in descending order
-	sort.Slice(pc.SortedPacks, func(i, j int) bool {
-		return pc.SortedPacks[i].Size > pc.SortedPacks[j].Size
-	})
+	// Perform the sorting concurrently
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		sort.Slice(pc.SortedPacks, func(i, j int) bool {
+			return pc.SortedPacks[i].Size > pc.SortedPacks[j].Size
+		})
+	}()
+
+	wg.Wait()
 }
 
 // CalculatePacks calculates the optimal combination of packs.
@@ -30,40 +43,54 @@ func (pc *PackCalculatorV1) CalculatePacks(order models.Order) []models.Pack {
 
 	remaining := order.Items
 	var res []models.Pack
-
 	var orderLargePacks []models.Pack
 
+	var wg sync.WaitGroup
 	largestPackSize := pc.SortedPacks[0].Size
 	quotient := remaining / largestPackSize
 	remainder := remaining % largestPackSize
 
-	// Add the required number of largest packs to the result
-	for i := 0; i < quotient; i++ {
-		orderLargePacks = append(orderLargePacks, models.Pack{Size: largestPackSize})
-	}
-	remaining = remainder
+	// Calculate the largest packs concurrently
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < quotient; i++ {
+			orderLargePacks = append(orderLargePacks, models.Pack{Size: largestPackSize})
+		}
+	}()
 
-	// Packing the remaining items with the next available packs
-	for remaining > 0 {
-		packAdded := false
+	// Calculate remaining packs concurrently
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		tempRemaining := remainder
+		for tempRemaining > 0 {
+			packAdded := false
 
-		// Try to use the largest pack that is less than or equal to the remaining items
-		for _, pack := range pc.SortedPacks {
-			if remaining >= pack.Size {
-				res = append(res, pack)
-				remaining -= pack.Size
-				packAdded = true
-				break
+			// Try to use the largest pack that is less than or equal to the remaining items
+			for _, pack := range pc.SortedPacks {
+				if tempRemaining >= pack.Size {
+					pc.mu.Lock()
+					res = append(res, pack)
+					pc.mu.Unlock()
+					tempRemaining -= pack.Size
+					packAdded = true
+					break
+				}
+			}
+
+			// If no pack was added, use the smallest available pack to handle the remaining items
+			if !packAdded {
+				smallestPack := pc.SortedPacks[len(pc.SortedPacks)-1]
+				pc.mu.Lock()
+				res = append(res, smallestPack)
+				pc.mu.Unlock()
+				tempRemaining -= smallestPack.Size
 			}
 		}
+	}()
 
-		// If no pack was added, use the smallest available pack to handle the remaining items
-		if !packAdded {
-			smallestPack := pc.SortedPacks[len(pc.SortedPacks)-1]
-			res = append(res, smallestPack)
-			remaining -= smallestPack.Size
-		}
-	}
+	wg.Wait()
 
 	// Append the optimized smaller packs to the result
 	result := append(orderLargePacks, optimizePacks(res, pc.SortedPacks)...)
